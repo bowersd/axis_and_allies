01dd8c4945ef2eb3778136c8f862c788c82cd240
sautedman <bowersd@ucla.edu>
2019-02-02 23:27:21 -0700

    import statements for probability/stats calculations

--- evaluate_forces.py
+++ evaluate_forces.py
@@ -1,3 +1,4 @@
+import probability as prb
 import math
 import random
 import itertools as it
@@ -11,69 +12,6 @@ import argparse
 #   chance of taking territory (planes can't take territory)
 #   planes potential targets for aa
 #expected cost of victory, financial damage wrought (requires unit costs, unit individuation)
-
-def binomial(n, N, p):
-    """calculate the binomial value of n successes out of N trials with p chance of success"""
-    #successes, trials, probability of success
-    return (math.factorial(N)/float(math.factorial(n)*math.factorial(N-n)))*(p**n)*((1-p)**(N-n))
-
-def binomial_curve(N, p):
-    """calculate the binomial value for all outcomes in N"""
-    return [binomial(i, N, p) for i in range(N+1)]
-
-def index_comb(*v):
-    """cartesian product of vector indices"""
-    return it.product(*[range(len(y)) for y in v])
-
-def product(*args):
-    return reduce(operator.mul, [a for a in args], 1)
-
-def binomial_joint(*np):
-    """
-    combine binomial curves with varying N and p
-    np = (N, p)
-    """
-    #np format different from what is used for grids (where indices provide p)
-    d = [0 for i in range(sum([x[0] for x in np])+1)] #domain
-    c = [binomial_curve(x[0], x[1]) for x in np] #curves
-    i = index_comb(*c) 
-    for j in i:
-        d[sum(j)] += product(*[c[k][x] for k, x in enumerate(j)])
-    return d
-
-#def curve_delta(c1, c2):
-#    """difference between c1, c2 in P(outcome) for each outcome"""
-#    return [c1[i]-c2[i] if i < len(c2) else c1[i] for i in range(len(c1))]
-
-def find_mode(*c):
-    """mode of a curve"""
-    i = 0
-    best = 0
-    for j in range(len(c)):
-        if c[j] > best:
-            i = j
-            best = c[j]
-    return i
-
-def mode_value(*c):
-    i = 0
-    best = 0
-    for j in range(len(c)):
-        if c[j] > best:
-            i = j
-            best = c[j]
-    return c[i]
-
-def find_mean(*c):
-    """mean of a curve"""
-    return sum([i*c[i] for i in range(len(c))])
-
-#def rout(n, *c):
-#    """what percent of the time does a curve exceed a threshold (winning on first roll)"""
-#    p = 0
-#    for i in range(len(c)):
-#        if i >= n: p+=c[i]
-#    return p
 
 def a_minus(n, *v):
     """deplete v (an army) from left to right (->0) until n is exhausted"""
@@ -115,7 +53,7 @@ def weight_transitions(a1, a2, trans):
     for s in trans: #start
         h[s] = {}
         a11, a21 = a_minus(sum(a1)-s[0], *a1), a_minus(sum(a2)-s[1], *a2) #take away losses
-        c1, c2 = binomial_joint(*[(a11[i], i/float(6)) for i in range(len(a11))]), binomial_joint(*[(a21[i], i/float(6)) for i in range(len(a21))])#get damage dealing capacity of each army
+        c1, c2 = prb.binomial_joint(*[(a11[i], i/float(6)) for i in range(len(a11))]), prb.binomial_joint(*[(a21[i], i/float(6)) for i in range(len(a21))])#get damage dealing capacity of each army
         overkill1, overkill2 = sum(c1[s[1]:]), sum(c2[s[0]:]) #p(army deals critical hit)
         for e in trans[s]: #end
             i, j = s[0]-e[0], s[1]-e[1] #damage dealt to a1, a2
@@ -189,8 +127,8 @@ def simulate(grid, trans, cap):
 
 def pprint_a(*c): 
     """print the joint binomial curve of an army"""
-    print("mode: {} ({}%)".format(find_mode(*c), round(mode_value(*c)*100, 1)))
-    print("mean: {}".format(round(find_mean(*c), 2)))
+    print("mode: {} ({}%)".format(prb.find_mode(*c), round(prb.mode_value(*c)*100, 1)))
+    print("mean: {}".format(round(prb.find_mean(*c), 2)))
     top = int(round(max(*c)*10, 0))
     labels = [str(i*10) for i in range(1, top+1)]
     pipes = ["|" for i in range(top)]
@@ -249,9 +187,9 @@ def sim_or_calc(n, m, a1, a2):
 if __name__ == "__main__":
     args = arg_parser().parse_args()
     if (args.defender and not args.attacker) :
-        pprint_a(*binomial_joint(*[(int(x[0]), int(x[1])/float(6)) for x in args.defender]))
+        pprint_a(*prb.binomial_joint(*[(int(x[0]), int(x[1])/float(6)) for x in args.defender]))
     elif (args.attacker and not args.defender):
-        pprint_a(*binomial_joint(*[(int(x[0]), int(x[1])/float(6)) for x in args.attacker]))
+        pprint_a(*prb.binomial_joint(*[(int(x[0]), int(x[1])/float(6)) for x in args.attacker]))
     else:
         #unordered representation for user input
         adict = {int(x[1]):int(x[0]) for x in args.attacker}
